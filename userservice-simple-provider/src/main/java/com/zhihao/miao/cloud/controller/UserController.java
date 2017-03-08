92400b6181fd253d2cea8e254a8f40b20371e74f
miaozhihao001 <1026145686@qq.com>
2017-03-08 23:44:37 +0800

    userservice-simple-provider/

--- userservice-simple-provider/src/main/java/com/zhihao/miao/cloud/controller/UserController.java
+++ userservice-simple-provider/src/main/java/com/zhihao/miao/cloud/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.zhihao.miao.cloud.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.zhihao.miao.cloud.entity.User;
 import com.zhihao.miao.cloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable Long id){
         return userService.findOne(id);
 
+    }
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        //第一个名字是注册到注册到Eureka上的服务名称
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo() {
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
     }
 
 }
