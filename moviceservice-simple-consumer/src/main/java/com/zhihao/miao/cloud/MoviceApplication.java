022c6f1e50813269a4f5b4fb44a227848c13962f
miaozhihao001 <1026145686@qq.com>
2017-03-12 23:24:32 +0800

    ribbon

--- moviceservice-simple-consumer/src/main/java/com/zhihao/miao/cloud/MoviceApplication.java
+++ moviceservice-simple-consumer/src/main/java/com/zhihao/miao/cloud/MoviceApplication.java
@@ -1,16 +1,24 @@
 package com.zhihao.miao.cloud;
 
+import com.zhihao.miao.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="microservice-provider-user", configuration = TestConfiguration.class)
 public class MoviceApplication {
 
+    //配置了@LoadBalanced表示默认的轮询策略，使用了@RibbonClient表明了可以定制服务的轮询策略，定制的策略就是configuration值，不要将
+    //configuration的类放到Application启动类的@ComponentScan路径下，这样会将所有服务的轮询策略都用configuration配置的策略所代替
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
---
+++ userservice-simple-provider2/src/main/java/com/zhihao/miao/cloud/service/UserService.java
@@ -0,0 +1,7 @@
+package com.zhihao.miao.cloud.service;
+
+import com.zhihao.miao.cloud.entity.User;
+
+public interface UserService {
+    User findOne(Long id);
+}
